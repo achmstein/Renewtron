b545a02299fcf659cde93c903e0c7dea37f05eb9
Ahmed El-Hady <Achmstein@outlook.com>
2025-10-22 15:53:08 +0300

    Fixes

---
+++ Renewtron/Data/Migrations/20251022123335_EnforceOneToOneRenewalRequest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Renewtron.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnforceOneToOneRenewalRequest : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_RenewalRequests_SearchResultId",
+                table: "RenewalRequests");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RenewalRequests_SearchResultId",
+                table: "RenewalRequests",
+                column: "SearchResultId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_RenewalRequests_SearchResultId",
+                table: "RenewalRequests");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RenewalRequests_SearchResultId",
+                table: "RenewalRequests",
+                column: "SearchResultId");
+        }
+    }
+}
